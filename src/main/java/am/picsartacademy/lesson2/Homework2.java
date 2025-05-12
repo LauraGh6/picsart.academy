83ac3c01723ec0df0d89d83a22bfeb9515df03c7
LauraGh6 <laura.ghulyan1996@gmail.com>
2025-05-12 19:37:29 +0400

    homework2 task4

---
+++ src/main/java/am/picsartacademy/lesson2/Homework2.java
@@ -0,0 +1,24 @@
+package am.picsartacademy.lesson2;
+
+public class Homework2 {
+    public static void main(String[] args) {
+        int[] array;
+        array = new int[50];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+
+        }
+
+        System.out.println("Numbers divisible by 5:");
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 5 == 0) {
+                System.out.println(array[i]);
+
+            }
+        }
+
+
+
+
+    }
+}
