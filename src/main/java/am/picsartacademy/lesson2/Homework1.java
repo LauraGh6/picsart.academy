823f0990fa0144c3587fa6cfa5c83e3438d90299
LauraGh6 <laura.ghulyan1996@gmail.com>
2025-05-12 15:45:45 +0400

    homework1 task2

--- src/main/java/am/picsartacademy/lesson2/Homework1.java
+++ src/main/java/am/picsartacademy/lesson2/Homework1.java
@@ -2,11 +2,18 @@ package am.picsartacademy.lesson2;
 
 public class Homework1 {
     public static void main(String[] args) {
-        //Task1
-        int[] array;
-        array = new int[2];
-        array[0] = 10;
-        array[1] = 20;
+        //Task2
+        int[] array = new int[1000];
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("setting value for index" + i + "where value is" + (i + 1));
+            array[i] = i + 1;
+
+
+        }
+
+
+
+
 
 
     }
