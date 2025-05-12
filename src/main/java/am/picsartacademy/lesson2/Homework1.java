ddbfa12635f4fc15c7ac9ea88871fa19d116b0ca
LauraGh6 <laura.ghulyan1996@gmail.com>
2025-05-12 15:15:01 +0400

    homework1 task1

---
+++ src/main/java/am/picsartacademy/lesson2/Homework1.java
@@ -0,0 +1,23 @@
+package am.picsartacademy.lesson2;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        //Task1
+        int[] array;
+        array = new int[2];
+        array[0] = 10;
+        array[1] = 20;
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
